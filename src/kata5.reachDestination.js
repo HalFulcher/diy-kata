63c9e3905c34d3ad2187eae2e35cc98e8ff6fe7a
Hallam Fulcher <hallam.fulcher@gmail.com>
2020-10-29 17:34:46 +0000

    ETA solved

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,11 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+
+const distDivSpeed = distance / speed;
+    function eta () {
+        return Math.round(distDivSpeed * 2) / 2;
+};
+
+return "I should be there in " + eta () + " hours."
+};
 
 module.exports = reachDestination;
